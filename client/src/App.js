e5b31c51e99ea3ae49303d3ebeec19877a5835c1
HALİL ÜNAL <125642299+Halil-Unal@users.noreply.github.com>
2023-04-03 17:58:57 +0300

    half

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import KaydedilenlerListesi from './Filmler/KaydedilenlerListesi';
+
+import Film from './Filmler/Film';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
-
+ 
   useEffect(() => {
     const FilmleriAl = () => {
       axios
         .get('http://localhost:5001/api/filmler') // Burayı Postman'le çalışın
         .then(response => {
+          setMovieList(response.data);
+          console.log(response);
           // Bu kısmı log statementlarıyla çalışın
           // ve burdan gelen response'u 'movieList' e aktarın
         })
@@ -27,10 +31,44 @@ export default function App () {
   };
 
   return (
-    <div>
-      <KaydedilenlerListesi list={[ /* Burası esnek */]} />
+    <BrowserRouter>
+      <div className="App">
+        <KaydedilenlerListesi list={[ /* Burası esnek */]} />
 
-      <div>Bu Div'i kendi Routelarınızla değiştirin</div>
-    </div>
+       
+
+        <div className="content">
+          <main>
+            <Switch>
+              <Route  path="/" >
+                <ul>
+                  {movieList.map(movie => (
+                    <li key={movie.id}>
+                    <h1><strong> </strong>{movie.title}</h1>
+                      <p><strong>Director:</strong>{movie.director}</p>
+                      <p><strong>Metascore:</strong>{movie.metascore}</p>
+                    
+                    </li>
+                    
+                  ))}
+                </ul>
+              </Route>
+            </Switch>
+            <Switch>
+              <Route path="/filmler/:id" component={Film} />
+             
+                <ul>
+                  {movieList.map(movie => (
+                    <li key={movie.id}>
+                      <Link to={`/filmler/${movie.id}`}>{movie.title}</Link>
+                    </li>
+                  ))}
+                </ul>
+            
+            </Switch>
+          </main>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
--- client/src/Filmler/Film.js
+++ client/src/Filmler/Film.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { useParams } from "react-router-dom";
 export default function Film(props) {
   const [movie, setMovie] = useState();
 
-  let id = 1;
+  //let id = 1;
   // URL'den alınan :id parametresini bu değişkene aktarın
-
+  //const id = props.match.params.id;
+  const { id } = useParams();
+  
   useEffect(() => {
     axios
       .get(`http://localhost:5001/api/filmler/${id}`) // Bu uç noktayı Postman'le çalışın
       .then(response => {
+        setMovie(response.data);
+        console.log(response);
           // Bu kısmı log statementlarıyla çalışın
           // ve burdan gelen response'u 'movie' e aktarın
       })
@@ -31,8 +35,12 @@ export default function Film(props) {
   const { title, director, metascore, stars } = movie;
 
   return (
+     
+  
+
     <div className="save-wrapper">
       <div className="movie-card">
+
         <h2>{title}</h2>
         <div className="movie-director">
           Director: <em>{director}</em>
